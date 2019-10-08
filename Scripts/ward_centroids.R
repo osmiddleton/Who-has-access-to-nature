1a53730a90d4eb16366aa12e084b131594a6a4b6
jkrward <j.k.ward@ncl.ac.uk>
2019-10-08 13:50:57 +0100

    starting to use ward data

---
+++ Scripts/ward_centroids.R
@@ -0,0 +1,18 @@
+# calculating distances from ward centroids to park centroids 
+library(sf)
+library(here)
+library(stringr)
+library(dplyr)
+wards <- st_read(dsn= here("Data", "Ward_boundaries/infuse_ward_lyr_2011/"), layer = "infuse_ward_lyr_2011")
+
+#plot(wards)
+
+#W_ward_codes <- as.data.frame(dplyr::select(wards, geo_code))
+
+#W_ward_codes <- select(wards,geo_code) %>% str_detect()
+
+EW_wards <- wards %>% filter(str_detect(geo_code, "^E.*") | str_detect(geo_code, "^W.*"))
+
+# get the centroids for the wards 
+EW_wards$Centroids <- st_centroid()
+# aggregate to county level
